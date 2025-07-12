8c14ff39cd1f2a31586154e6a7d5fc0d47d90cbf
kuya-carlo <106532351+kuya-carlo@users.noreply.github.com>
2025-07-13 05:55:16 +0800

    Fixed CI errors due to pnpm in ci.yml, again.(5)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,9 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
-console.log("🔍 SUPABASE_URL:", process.env.SUPABASE_URL);
-console.log("🔍 SUPABASE_ANON_KEY:", process.env.SUPABASE_ANON_KEY);
-
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
   throw new Error("Supabase environment variables are not defined.");
 }
