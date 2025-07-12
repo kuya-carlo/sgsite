9439e57b465892a81da3cb04ee8376946bd1590f
kuya-carlo <106532351+kuya-carlo@users.noreply.github.com>
2025-07-13 06:00:12 +0800

    Added Vercel's analytics library

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import '@/styles/global.scss';
 import { ReactNode } from 'react';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
