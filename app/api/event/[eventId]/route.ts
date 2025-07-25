34a04822e646ea81e7edb1c538a1dbebcca23f76
kuya-carlo <106532351+kuya-carlo@users.noreply.github.com>
2025-07-25 15:19:07 +0800

    fix: Fixed API logic errors

--- app/api/event/[eventId]/route.ts
+++ app/api/event/[eventId]/route.ts
@@ -1,5 +1,5 @@
 // app/api/event/[eventId]
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { EventRecord } from "@/types/data";
 import { getEventData } from "@/lib/data";
 
@@ -7,7 +7,7 @@ type Props = {
   params: Promise<{ eventId: string }>;
 };
 
-export async function GET({ params }: Props) {
+export async function GET(_request: NextRequest, { params }: Props) {
   const { eventId: event_id } = await params;
   const { data: rawevent, error: event_errors } = await getEventData(event_id);
   const event_data: EventRecord | null = rawevent as EventRecord | null;
